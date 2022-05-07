f0d69a508290dc5365cac739464ccbaa6130f4bb
Webshilp <info@webshilp.com>
2022-05-07 13:42:31 +0530

    environment to developement

--- index.php
+++ index.php
@@ -17,12 +17,12 @@ chdir(__DIR__);
 
 // Load our paths config file
 // This is the line that might need to be changed, depending on your folder structure.
-$pathsConfig = FCPATH . '/app/Config/Paths.php';
+$pathsConfig = FCPATH . 'app/Config/Paths.php';
 // ^^^ Change this if you move your application folder
 require realpath($pathsConfig) ?: $pathsConfig;
 
 $paths = new Config\Paths();
-
+define('ENVIRONMENT', 'development');
 // Location of the framework bootstrap file.
 $bootstrap = rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
 $app       = require realpath($bootstrap) ?: $bootstrap;
