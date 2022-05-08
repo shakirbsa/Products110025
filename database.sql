05b08618aec21053422f2451cb378a8b71b3ec6e
Webshilp <info@webshilp.com>
2022-05-08 19:55:05 +0530

    Sql chnages

--- database.sql
+++ database.sql
@@ -1,11 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2022 at 04:23 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `products`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `product_name` varchar(100) NOT NULL,
   `product_price` varchar(11) NOT NULL,
   `product_description` varchar(500) NOT NULL,
-  `product_image` text NOT NULL,
+  `product_image` varchar(500) NOT NULL,
   `created_by` int(11) NOT NULL,
   `created_on` datetime NOT NULL DEFAULT current_timestamp(),
-  `is_active` tinyint(4) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`id`)
+  `is_active` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product_name`, `product_price`, `product_description`, `product_image`, `created_by`, `created_on`, `is_active`) VALUES
+(55, 'LED Digital Multi-Colour Dial Kid\'s Watch', '500', 'Stylish and Attractive M2 Band Shape LED Digital Multi-Colour Dial Kid\'s Watch ,Best Return Gift in birthday and other parties.', '[\"165201973816490706277d21af3334.jpeg\",\"165201973885587616277d21af3529.jpeg\"]', 0, '2022-05-08 19:52:19', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
